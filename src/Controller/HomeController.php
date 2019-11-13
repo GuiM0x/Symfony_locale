e07a15ceb44bec14060dcf2e371fac3adb7bfa9f
GuiM0x <martox.lintox@gmail.com>
2019-11-13 21:56:16 +0100

    Routing & Local config updated + symfony apache pack

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,11 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Route("/", requirements={"_locale": "%app.supported_locales%"}, name="home_")
+ */
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/{_locale}", name="index")
      */
     public function index()
     {
